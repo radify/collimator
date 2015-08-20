0763b84ae73a0e36c6bef6993e4e6fe47917fc7d
John F. Mercer <john@radify.io>
2015-08-20 10:07:24 -0400

    Adds jsdoc to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp     = require('gulp');
 var istanbul = require('gulp-istanbul');
 var jasmine  = require('gulp-jasmine');
 var jscs     = require('gulp-jscs');
+var jsdoc    = require('gulp-jsdoc');
 
 var src  = './src/**/*.js';
 var spec = './spec/**/*.js';
@@ -9,6 +10,11 @@ var spec = './spec/**/*.js';
 gulp.task('test', function() {
   return gulp.src(spec)
       .pipe(jasmine());
+});
+
+gulp.task('jsdoc', function() {
+  return gulp.src(src)
+	  .pipe(jsdoc('dist/docs'));
 });
 
 gulp.task('coverage', function() {
@@ -32,7 +38,7 @@ gulp.task('style', function() {
     }));
 });
 
-gulp.task('default', ['style', 'test']);
+gulp.task('default', ['style', 'test', 'jsdoc']);
 
 gulp.task('dev', function() {
   gulp.watch([src, spec], ['default']);
