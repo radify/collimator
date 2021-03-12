f7673c3d178d4f0713ab268515fdb46d16115128
Warren Seymour <warren@fountainhead.tech>
2021-03-12 18:01:47 +0000

    feat: Ensure stable table ordering across PostgreSQL versions
    The default datatype of `tables.table_name` changed from `varchar` to `name` (which uses the `"C"` collation) in PostgreSQL 12. To ensure a backward-compatible sort order, we explicitly use this collation to cater for older versions.

--- src/inspectors/tables.sql
+++ src/inspectors/tables.sql
@@ -1,6 +1,6 @@
 SELECT
   t.table_name AS name,
-  array_agg(ccu.column_name::text) AS "primaryKeys"
+  array_agg(ccu.column_name::text ORDER BY ccu.column_name COLLATE "C") AS "primaryKeys"
 
 FROM information_schema.tables AS t
 
@@ -12,4 +12,6 @@ ON ccu.constraint_name = tc.constraint_name
 
 WHERE t.table_schema = 'public'
 
-GROUP BY t.table_name;
+GROUP BY t.table_name
+
+ORDER BY t.table_name COLLATE "C";
